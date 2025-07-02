553fa373484b0aac77bc08134cb52bdd4a43b5e2
tn-lorenz <lorenz.tim-niklas@protonmail.com>
2025-07-02 03:26:40 +0200

    fixed imports + applied same member order for BedBlock

--- pumpkin/src/block/blocks/bed.rs
+++ pumpkin/src/block/blocks/bed.rs
@@ -6,7 +6,6 @@ use pumpkin_data::entity::EntityType;
 use pumpkin_data::tag::{RegistryKey, get_tag_values};
 use pumpkin_data::{Block, BlockDirection};
 use pumpkin_data::{BlockState, block_properties::BedPart};
-use pumpkin_macros::send_cancellable;
 use pumpkin_protocol::java::server::play::SUseItemOn;
 use pumpkin_registry::VanillaDimensionType;
 use pumpkin_util::GameMode;
@@ -23,7 +22,6 @@ use crate::block::pumpkin_block::{BlockMetadata, PumpkinBlock};
 use crate::entity::player::Player;
 use crate::entity::{Entity, EntityBase};
 use crate::plugin::player::player_bed_enter::{BedEnterResult, PlayerBedEnterEvent};
-use crate::plugin::player::player_death::PlayerDeathEvent;
 use crate::server::Server;
 use crate::world::World;
 
@@ -42,107 +40,6 @@ impl BlockMetadata for BedBlock {
 
 #[async_trait]
 impl PumpkinBlock for BedBlock {
-    async fn can_place_at(
-        &self,
-        _server: Option<&Server>,
-        world: Option<&World>,
-        _block_accessor: &dyn BlockAccessor,
-        player: Option<&Player>,
-        _block: &Block,
-        block_pos: &BlockPos,
-        _face: BlockDirection,
-        _use_item_on: Option<&SUseItemOn>,
-    ) -> bool {
-        if let Some(player) = player {
-            let facing = player.living_entity.entity.get_horizontal_facing();
-            let world = world.unwrap();
-            return world.get_block_state(block_pos).await.replaceable()
-                && world
-                    .get_block_state(&block_pos.offset(facing.to_offset()))
-                    .await
-                    .replaceable();
-        }
-        false
-    }
-
-    async fn on_place(
-        &self,
-        _server: &Server,
-        _world: &World,
-        player: &Player,
-        block: &Block,
-        _block_pos: &BlockPos,
-        _face: BlockDirection,
-        _replacing: BlockIsReplacing,
-        _use_item_on: &SUseItemOn,
-    ) -> BlockStateId {
-        let mut bed_props = BedProperties::default(block);
-
-        bed_props.facing = player.living_entity.entity.get_horizontal_facing();
-        bed_props.part = BedPart::Foot;
-
-        bed_props.to_state_id(block)
-    }
-
-    async fn placed(
-        &self,
-        world: &Arc<World>,
-        block: &Block,
-        state_id: BlockStateId,
-        block_pos: &BlockPos,
-        _old_state_id: BlockStateId,
-        _notify: bool,
-    ) {
-        let bed_entity = BedBlockEntity::new(*block_pos);
-        world.add_block_entity(Arc::new(bed_entity)).await;
-
-        let mut bed_head_props = BedProperties::default(block);
-        bed_head_props.facing = BedProperties::from_state_id(state_id, block).facing;
-        bed_head_props.part = BedPart::Head;
-
-        let bed_head_pos = block_pos.offset(bed_head_props.facing.to_offset());
-        world
-            .set_block_state(
-                &bed_head_pos,
-                bed_head_props.to_state_id(block),
-                BlockFlags::NOTIFY_ALL | BlockFlags::SKIP_BLOCK_ADDED_CALLBACK,
-            )
-            .await;
-
-        let bed_head_entity = BedBlockEntity::new(bed_head_pos);
-        world.add_block_entity(Arc::new(bed_head_entity)).await;
-    }
-
-    async fn broken(
-        &self,
-        block: &Block,
-        player: &Arc<Player>,
-        block_pos: BlockPos,
-        _server: &Server,
-        world: Arc<World>,
-        state: BlockState,
-    ) {
-        let bed_props = BedProperties::from_state_id(state.id, block);
-        let other_half_pos = if bed_props.part == BedPart::Head {
-            block_pos.offset(bed_props.facing.opposite().to_offset())
-        } else {
-            block_pos.offset(bed_props.facing.to_offset())
-        };
-
-        world
-            .break_block(
-                &other_half_pos,
-                Some(player.clone()),
-                if player.gamemode.load() == GameMode::Creative {
-                    BlockFlags::SKIP_DROPS | BlockFlags::NOTIFY_NEIGHBORS
-                } else {
-                    BlockFlags::NOTIFY_NEIGHBORS
-                },
-            )
-            .await;
-    }
-
-    // TODO: Fix - I hate ts so much...why have a send_cancellable! macro when you pretty much never use an Arc
     async fn normal_use(
         &self,
         block: &Block,
@@ -368,6 +265,106 @@ impl PumpkinBlock for BedBlock {
                 Self::set_occupied(true, world, block, &block_pos, state_id).await;
             }
         }
+    }
+
+    async fn on_place(
+        &self,
+        _server: &Server,
+        _world: &World,
+        player: &Player,
+        block: &Block,
+        _block_pos: &BlockPos,
+        _face: BlockDirection,
+        _replacing: BlockIsReplacing,
+        _use_item_on: &SUseItemOn,
+    ) -> BlockStateId {
+        let mut bed_props = BedProperties::default(block);
+
+        bed_props.facing = player.living_entity.entity.get_horizontal_facing();
+        bed_props.part = BedPart::Foot;
+
+        bed_props.to_state_id(block)
+    }
+
+    async fn can_place_at(
+        &self,
+        _server: Option<&Server>,
+        world: Option<&World>,
+        _block_accessor: &dyn BlockAccessor,
+        player: Option<&Player>,
+        _block: &Block,
+        block_pos: &BlockPos,
+        _face: BlockDirection,
+        _use_item_on: Option<&SUseItemOn>,
+    ) -> bool {
+        if let Some(player) = player {
+            let facing = player.living_entity.entity.get_horizontal_facing();
+            let world = world.unwrap();
+            return world.get_block_state(block_pos).await.replaceable()
+                && world
+                    .get_block_state(&block_pos.offset(facing.to_offset()))
+                    .await
+                    .replaceable();
+        }
+        false
+    }
+
+    async fn placed(
+        &self,
+        world: &Arc<World>,
+        block: &Block,
+        state_id: BlockStateId,
+        block_pos: &BlockPos,
+        _old_state_id: BlockStateId,
+        _notify: bool,
+    ) {
+        let bed_entity = BedBlockEntity::new(*block_pos);
+        world.add_block_entity(Arc::new(bed_entity)).await;
+
+        let mut bed_head_props = BedProperties::default(block);
+        bed_head_props.facing = BedProperties::from_state_id(state_id, block).facing;
+        bed_head_props.part = BedPart::Head;
+
+        let bed_head_pos = block_pos.offset(bed_head_props.facing.to_offset());
+        world
+            .set_block_state(
+                &bed_head_pos,
+                bed_head_props.to_state_id(block),
+                BlockFlags::NOTIFY_ALL | BlockFlags::SKIP_BLOCK_ADDED_CALLBACK,
+            )
+            .await;
+
+        let bed_head_entity = BedBlockEntity::new(bed_head_pos);
+        world.add_block_entity(Arc::new(bed_head_entity)).await;
+    }
+
+    async fn broken(
+        &self,
+        block: &Block,
+        player: &Arc<Player>,
+        block_pos: BlockPos,
+        _server: &Server,
+        world: Arc<World>,
+        state: BlockState,
+    ) {
+        let bed_props = BedProperties::from_state_id(state.id, block);
+        let other_half_pos = if bed_props.part == BedPart::Head {
+            block_pos.offset(bed_props.facing.opposite().to_offset())
+        } else {
+            block_pos.offset(bed_props.facing.to_offset())
+        };
+
+        world
+            .break_block(
+                &other_half_pos,
+                Some(player.clone()),
+                if player.gamemode.load() == GameMode::Creative {
+                    BlockFlags::SKIP_DROPS | BlockFlags::NOTIFY_NEIGHBORS
+                } else {
+                    BlockFlags::NOTIFY_NEIGHBORS
+                },
+            )
+            .await;
     }
 }
 
