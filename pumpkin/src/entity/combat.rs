a7a27b1ce9159fde7767dfce43ea95dfe916cef7
tn-lorenz <lorenz.tim-niklas@protonmail.com>
2025-07-05 01:57:16 +0200

    Initial implementation of the Classic COmbatProfile and its methods

--- pumpkin/src/entity/combat.rs
+++ pumpkin/src/entity/combat.rs
@@ -1,8 +1,16 @@
+use std::sync::Arc;
+use std::sync::atomic::Ordering;
+use async_trait::async_trait;
+use dashmap::DashMap;
+use once_cell::sync::Lazy;
+use rand::Rng;
+use uuid::Uuid;
 use pumpkin_data::{
     particle::Particle,
     sound::{Sound, SoundCategory},
 };
 use pumpkin_protocol::{codec::var_int::VarInt, java::client::play::CEntityVelocity};
+use pumpkin_util::math::square;
 use pumpkin_util::math::vector3::Vector3;
 
 use crate::{
@@ -120,5 +128,169 @@ pub async fn player_attack_sound(pos: &Vector3<f64>, world: &World, attack_type:
                 .play_sound(Sound::EntityPlayerAttackWeak, SoundCategory::Players, pos)
                 .await;
         }
+    }
+}
+
+pub static COMBAT_PROFILES: Lazy<DashMap<Uuid, Arc<dyn CombatProfile>>> = Lazy::new(|| DashMap::new());
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum CombatType {
+    Legacy,
+    Classic,
+    Modern,
+}
+
+#[async_trait]
+pub trait CombatProfile {
+    async fn apply_attack_knockback(&self, target: Arc<Entity>, strength: f64);
+    async fn receive_knockback(&self, entity: Arc<Entity>, double_1: i32, double_2: i32);
+    async fn combat_type(&self) -> CombatType;
+    async fn friction(&self) -> f64;
+    async fn horizontal_kb(&self) -> f64;
+    async fn vertical_kb(&self) -> f64;
+    async fn vertical_limit(&self) -> f64;
+    async fn extra_horizontal_kb(&self) -> f64;
+    async fn extra_vertical_kb(&self) -> f64;
+}
+
+pub struct ClassicProfile {
+    pub friction: f64,
+    pub horizontal_kb: f64,
+    pub vertical_kb: f64,
+    pub vertical_limit: f64,
+    pub extra_horizontal_kb: f64,
+    pub extra_vertical_kb: f64,
+}
+
+impl CombatProfile for ClassicProfile {
+    // TODO: send update packet, but maybe do that when this fn is called
+    /// Getting called from an attacker, when attacking an entity
+    async fn apply_attack_knockback(&self, target: Arc<Entity>, strength: f64) {
+        let yaw: f64 = target.yaw.load() as f64;
+        let yaw_rad = yaw.to_radians();
+
+        // The `extra_horizontal_kb` is 0.5 and `extra_vertical_kb` 0.1 by default in java mc 1.8
+        let knockback_x = -yaw_rad.sin() * strength * self.extra_horizontal_kb;
+        let knockback_z =  yaw_rad.cos() * strength * self.extra_horizontal_kb;
+        let knockback_y = self.extra_vertical_kb;
+
+        let mut velocity = target.velocity.load();
+
+        velocity.x += knockback_x;
+        velocity.y += knockback_y;
+        velocity.z += knockback_z;
+
+        velocity.x *= 0.6;
+        velocity.z *= 0.6;
+
+        target.velocity.store(velocity);
+
+        // TODO: DON'T reset sprinting of the target, but the attacker
+        target.sprinting.store(false, Ordering::Relaxed);
+    }
+
+    /// Getting called on a target, when being attacked
+    // the `float p_70653_2_` from java is dead code, so I removed it
+    async fn receive_knockback(&self, target: Arc<Entity>, knockback_x: f64, knockback_z: f64) {
+        let mut rng = rand::rng();
+        let knockback_resistance = self.get_attribute_value("knockback_resistance").await;
+
+        if rng.random::<f64>() >= knockback_resistance {
+            target.on_ground = false;
+
+            let magnitude = (square(knockback_x) + square(knockback_z)).sqrt();
+            let mut velocity = target.velocity.load();
+
+            // `friction` is 2.0 by default in java mc 1.8
+            velocity.x /= self.friction;
+            velocity.y /= self.friction;
+            velocity.z /= self.friction;
+
+            // `horizontal_kb` and `vertical_kb` are 0.4 by default in java mc 1.8
+            velocity.x -= knockback_x / magnitude * self.horizontal_kb;
+            velocity.y += self.vertical_kb;
+            velocity.z -= knockback_z / magnitude * self.horizontal_kb;
+
+            // `vertical_limit` is 0.4000000059604645 by default in java mc 1.8
+            if velocity.y > self.vertical_limit {
+                velocity.y = self.vertical_limit;
+            }
+        }
+    }
+
+    async fn combat_type(&self) -> CombatType {
+        CombatType::Classic
+    }
+
+    async fn friction(&self) -> f64 {
+        self.friction
+    }
+
+    async fn horizontal_kb(&self) -> f64 {
+        self.horizontal_kb
+    }
+
+    async fn vertical_kb(&self) -> f64 {
+        self.vertical_kb
+    }
+
+    async fn vertical_limit(&self) -> f64 {
+        self.vertical_limit
+    }
+
+    async fn extra_horizontal_kb(&self) -> f64 {
+        self.extra_horizontal_kb
+    }
+
+    async fn extra_vertical_kb(&self) -> f64 {
+        self.extra_vertical_kb
+    }
+}
+
+// TODO: Hier sind andere Werte wichtig -> ändern
+pub struct ModernProfile {
+    pub friction: f64,
+    pub horizontal_kb: f64,
+    pub vertical_kb: f64,
+    pub vertical_limit: f64,
+    pub extra_horizontal_kb: f64,
+    pub extra_vertical_kb: f64,
+}
+
+impl CombatProfile for ModernProfile {
+    async fn apply_attack_knockback(&self, target: Arc<Player>, strength: f64) {
+        todo!()
+    }
+
+    async fn receive_knockback(&self, entity: Arc<Entity>, double_1: i32, double_2: i32) {
+        todo!()
+    }
+
+    async fn combat_type(&self) -> CombatType {
+        CombatType::Modern
+    }
+
+    async fn friction(&self) -> f64 {
+        self.friction
+    }
+
+    async fn horizontal_kb(&self) -> f64 {
+        self.horizontal_kb
+    }
+
+    async fn vertical_kb(&self) -> f64 {
+        self.vertical_kb
+    }
+
+    async fn vertical_limit(&self) -> f64 {
+        self.vertical_limit
+    }
+
+    async fn extra_horizontal_kb(&self) -> f64 {
+        self.extra_horizontal_kb
+    }
+
+    async fn extra_vertical_kb(&self) -> f64 {
+        self.extra_vertical_kb
     }
 }
