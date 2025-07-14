7ebc507658ecce05955a59eeeb609383947c6f30
tn-lorenz <lorenz.tim-niklas@protonmail.com>
2025-07-14 16:20:20 +0200

    Added from_persistent fn for ease of use

--- pumpkin/src/plugin/api/persistence/mod.rs
+++ pumpkin/src/plugin/api/persistence/mod.rs
@@ -29,10 +29,12 @@ impl NamespacedKey {
     #[allow(dead_code)]
     pub(crate) fn new(namespace: &str, key: &str) -> Result<Self, NamespacedKeyError> {
         if !namespace.is_ascii() {
+            #[cfg(debug_assertions)]
             log::error!("Invalid namespace: '{namespace}' is not pure ASCII.");
             return Err(NamespacedKeyError::NonAsciiNamespace);
         }
         if !key.is_ascii() {
+            #[cfg(debug_assertions)]
             log::error!("Invalid key: '{key}' is not pure ASCII.");
             return Err(NamespacedKeyError::NonAsciiKey);
         }
@@ -53,7 +55,7 @@ macro_rules! ns_key {
     ($value:expr) => {
         match $crate::plugin::NamespacedKey::new(env!("CARGO_PKG_NAME"), $value) {
             Ok(key) => key,
-            Err(e) => panic!("Invalid key: {:?}", e),
+            Err(e) => panic!("ns_key! macro failed: {:?}", e);
         }
     };
 }
@@ -64,12 +66,12 @@ macro_rules! ns_key {
 #[allow(dead_code)]
 #[derive(Default, Debug)]
 pub struct PersistentDataContainer {
-    pub data: DashMap<NamespacedKey, PersistentValue>,
+    pub data: DashMap<NamespacedKey, PersistentDataType>,
 }
 
 #[allow(dead_code)]
 #[derive(PartialEq, Debug, Clone, Serialize, Deserialize)]
-pub enum PersistentValue {
+pub enum PersistentDataType {
     Bool(bool),
     String(String),
     Char(char),
@@ -82,7 +84,7 @@ pub enum PersistentValue {
     F32(f32),
     F64(f64),
     Bytes(Vec<u8>),
-    List(Vec<PersistentValue>),
+    List(Vec<PersistentDataType>),
 }
 
 impl PersistentDataContainer {
@@ -97,16 +99,21 @@ impl PersistentDataContainer {
     }
 
     #[must_use]
-    pub fn get(&self, key: &NamespacedKey) -> Option<PersistentValue> {
+    pub fn get(&self, key: &NamespacedKey) -> Option<PersistentDataType> {
         self.data.get(key).map(|v| v.clone())
     }
 
-    pub fn insert(&self, key: &NamespacedKey, value: PersistentValue) {
+    #[must_use]
+    pub fn get_as<T: FromPersistentDataType>(&self, key: &NamespacedKey) -> Option<T> {
+        self.get(key).and_then(|v| T::from_persistent(&v))
+    }
+
+    pub fn insert(&self, key: &NamespacedKey, value: PersistentDataType) {
         self.data.insert(key.clone(), value);
     }
 
     #[must_use]
-    pub fn remove(&self, key: &NamespacedKey) -> Option<(NamespacedKey, PersistentValue)> {
+    pub fn remove(&self, key: &NamespacedKey) -> Option<(NamespacedKey, PersistentDataType)> {
         self.data.remove(key)
     }
 
@@ -115,7 +122,7 @@ impl PersistentDataContainer {
         self.data.contains_key(key)
     }
 
-    pub fn iter(&self) -> impl Iterator<Item = (NamespacedKey, PersistentValue)> + '_ {
+    pub fn iter(&self) -> impl Iterator<Item = (NamespacedKey, PersistentDataType)> + '_ {
         self.data
             .iter()
             .map(|entry| (entry.key().clone(), entry.value().clone()))
@@ -128,11 +135,12 @@ pub trait HasPersistentContainer {
 
 pub trait PersistentDataHolder {
     fn clear(&self);
-    fn get(&self, key: &NamespacedKey) -> Option<PersistentValue>;
-    fn insert(&self, key: &NamespacedKey, value: PersistentValue);
-    fn remove(&self, key: &NamespacedKey) -> Option<PersistentValue>;
+    fn get(&self, key: &NamespacedKey) -> Option<PersistentDataType>;
+    fn get_as<T: FromPersistentDataType>(&self, key: &NamespacedKey) -> Option<T>;
+    fn insert(&self, key: &NamespacedKey, value: PersistentDataType);
+    fn remove(&self, key: &NamespacedKey) -> Option<PersistentDataType>;
     fn contains_key(&self, key: &NamespacedKey) -> bool;
-    fn iter(&self) -> Box<dyn Iterator<Item = (NamespacedKey, PersistentValue)> + '_>;
+    fn iter(&self) -> Box<dyn Iterator<Item = (NamespacedKey, PersistentDataType)> + '_>;
 }
 
 impl<T: HasPersistentContainer> PersistentDataHolder for T {
@@ -140,15 +148,19 @@ impl<T: HasPersistentContainer> PersistentDataHolder for T {
         self.persistent_container().clear();
     }
 
-    fn get(&self, key: &NamespacedKey) -> Option<PersistentValue> {
+    fn get(&self, key: &NamespacedKey) -> Option<PersistentDataType> {
         self.persistent_container().get(key)
     }
 
-    fn insert(&self, key: &NamespacedKey, value: PersistentValue) {
+    fn get_as<U: FromPersistentDataType>(&self, key: &NamespacedKey) -> Option<U> {
+        self.get(key).and_then(|v| U::from_persistent(&v))
+    }
+
+    fn insert(&self, key: &NamespacedKey, value: PersistentDataType) {
         self.persistent_container().insert(key, value);
     }
 
-    fn remove(&self, key: &NamespacedKey) -> Option<PersistentValue> {
+    fn remove(&self, key: &NamespacedKey) -> Option<PersistentDataType> {
         self.persistent_container().remove(key).map(|(_, v)| v)
     }
 
@@ -156,7 +168,128 @@ impl<T: HasPersistentContainer> PersistentDataHolder for T {
         self.persistent_container().contains_key(key)
     }
 
-    fn iter(&self) -> Box<dyn Iterator<Item = (NamespacedKey, PersistentValue)> + '_> {
+    fn iter(&self) -> Box<dyn Iterator<Item = (NamespacedKey, PersistentDataType)> + '_> {
         Box::new(self.persistent_container().iter())
+    }
+}
+
+pub trait FromPersistentDataType: Sized {
+    fn from_persistent(value: &PersistentDataType) -> Option<Self>;
+}
+
+impl FromPersistentDataType for bool {
+    fn from_persistent(value: &PersistentDataType) -> Option<Self> {
+        match value {
+            PersistentDataType::Bool(v) => Some(*v),
+            _ => None,
+        }
+    }
+}
+
+impl FromPersistentDataType for String {
+    fn from_persistent(value: &PersistentDataType) -> Option<Self> {
+        match value {
+            PersistentDataType::String(v) => Some(v.clone()),
+            _ => None,
+        }
+    }
+}
+
+impl FromPersistentDataType for char {
+    fn from_persistent(value: &PersistentDataType) -> Option<Self> {
+        match value {
+            PersistentDataType::Char(v) => Some(*v),
+            _ => None,
+        }
+    }
+}
+
+impl FromPersistentDataType for i32 {
+    fn from_persistent(value: &PersistentDataType) -> Option<Self> {
+        match value {
+            PersistentDataType::I32(v) => Some(*v),
+            _ => None,
+        }
+    }
+}
+
+impl FromPersistentDataType for i64 {
+    fn from_persistent(value: &PersistentDataType) -> Option<Self> {
+        match value {
+            PersistentDataType::I64(v) => Some(*v),
+            _ => None,
+        }
+    }
+}
+
+impl FromPersistentDataType for u8 {
+    fn from_persistent(value: &PersistentDataType) -> Option<Self> {
+        match value {
+            PersistentDataType::U8(v) => Some(*v),
+            _ => None,
+        }
+    }
+}
+
+impl FromPersistentDataType for u16 {
+    fn from_persistent(value: &PersistentDataType) -> Option<Self> {
+        match value {
+            PersistentDataType::U16(v) => Some(*v),
+            _ => None,
+        }
+    }
+}
+
+impl FromPersistentDataType for u32 {
+    fn from_persistent(value: &PersistentDataType) -> Option<Self> {
+        match value {
+            PersistentDataType::U32(v) => Some(*v),
+            _ => None,
+        }
+    }
+}
+
+impl FromPersistentDataType for u64 {
+    fn from_persistent(value: &PersistentDataType) -> Option<Self> {
+        match value {
+            PersistentDataType::U64(v) => Some(*v),
+            _ => None,
+        }
+    }
+}
+
+impl FromPersistentDataType for f32 {
+    fn from_persistent(value: &PersistentDataType) -> Option<Self> {
+        match value {
+            PersistentDataType::F32(v) => Some(*v),
+            _ => None,
+        }
+    }
+}
+
+impl FromPersistentDataType for f64 {
+    fn from_persistent(value: &PersistentDataType) -> Option<Self> {
+        match value {
+            PersistentDataType::F64(v) => Some(*v),
+            _ => None,
+        }
+    }
+}
+
+impl FromPersistentDataType for Vec<u8> {
+    fn from_persistent(value: &PersistentDataType) -> Option<Self> {
+        match value {
+            PersistentDataType::Bytes(v) => Some(v.clone()),
+            _ => None,
+        }
+    }
+}
+
+impl FromPersistentDataType for Vec<PersistentDataType> {
+    fn from_persistent(value: &PersistentDataType) -> Option<Self> {
+        match value {
+            PersistentDataType::List(v) => Some(v.clone()),
+            _ => None,
+        }
     }
 }
