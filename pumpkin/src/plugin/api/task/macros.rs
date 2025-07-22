88c25b073759294113afea5f7b298d7ea6197236
tn-lorenz <lorenz.tim-niklas@protonmail.com>
2025-07-23 00:09:43 +0200

    hariy ballz

--- pumpkin/src/plugin/api/task/macros.rs
+++ pumpkin/src/plugin/api/task/macros.rs
@@ -19,7 +19,7 @@ macro_rules! run_task_later {
 
 #[macro_export]
 macro_rules! run_task_timer {
-    ($server:expr, $interval_ticks:expr, $body:tt) => {{
+    ($server:expr, $interval_ticks:expr, $($body:tt)*) => {{
         use async_trait::async_trait;
         use std::sync::{
             Arc,
@@ -34,16 +34,15 @@ macro_rules! run_task_timer {
         #[async_trait]
         impl TaskHandler for InlineHandler {
             async fn run(&self) {
-                let cancel = || {
-                    self.cancel_flag.store(true, Ordering::Relaxed);
+                let cancel_flag = self.cancel_flag.clone();
+
+                let cancel = move || {
+                    cancel_flag.store(true, Ordering::Relaxed);
                 };
 
-                let cancel_ref = &cancel;
-
                 async move {
-                    #[allow(unused_variables)]
-                    let cancel = cancel_ref;
-                    $body
+                    let cancel = &cancel;
+                    $($body)*
                 }
                 .await;
             }
